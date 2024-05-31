f53c6f31315a44592fc76bf850b17b9a6abbef1b
Brendon <brendonnewton0@gmail.com>
2024-05-31 13:51:32 -0500

    documentation for bubble_sort and insertion_sort api functions

--- api/src/sorting/implementations/mod.rs
+++ api/src/sorting/implementations/mod.rs
@@ -1,22 +1,48 @@
 use super::super::structs::{Cycle, SortingInput, SortingOutput, Step};
 
+/***
+ * Bubble Sort
+ *
+ * Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order.
+ * The pass through the list is repeated until the list is sorted.
+ *
+ * Time Complexity:
+ * - Best: O(n)
+ * - Average: O(n^2)
+ * - Worst: O(n^2)
+ *
+ * Space Complexity: O(1)
+ *
+ * Stable: Yes
+ *
+ * @param input: SortingInput
+ * @returns SortingOutput
+ */
 pub fn bubble_sort(input: SortingInput) -> SortingOutput {
+    // Initialize variables
     let mut arr = input.arr;
     let n = arr.len();
     let mut cycles: Vec<Cycle> = Vec::new();
 
+    // Iterate through the array
     for i in 0..n {
+        // Initialize variables
         let mut steps = Vec::new();
         let mut swapped = false;
+        // Compare adjacent elements and swap if they are in the wrong order, stopping one element shorter each loop
         for j in 0..n - i - 1 {
+            // If the element is greater than the next element, swap them
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j + 1);
                 swapped = true;
+                // Add the step to the cycle
                 steps.push(Step {
                     swapped: true,
                     compared: vec![(j, j + 1)],
                 });
+            // If the element is not greater than the next element, do not swap them
             } else {
+                // Add the step to the cycle
                 steps.push(Step {
                     swapped: false,
                     compared: vec![(j, j + 1)],
@@ -24,59 +50,94 @@ pub fn bubble_sort(input: SortingInput) -> SortingOutput {
             }
         }
 
+        // If the steps are not empty, add the cycle to the cycles
         if !steps.is_empty() {
             cycles.push(Cycle { cycle: steps });
         }
 
+        // If no swaps were made, the array is already sorted
         if !swapped {
             break;
         }
     }
 
+    // If no cycles were made, add an empty cycle
     if cycles.is_empty() {
         cycles.push(Cycle { cycle: Vec::new() });
     }
 
+    // Return the output
     SortingOutput {
         sort: "Bubble".to_string(),
         cycles: cycles,
     }
 }
 
+/***
+ * Insertion Sort
+ *
+ * Insertion sort is a simple sorting algorithm that builds the final sorted array one item at a time.
+ * It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+ *
+ * Time Complexity:
+ * - Best: O(n)
+ * - Average: O(n^2)
+ * - Worst: O(n^2)
+ *
+ * Space Complexity: O(1)
+ *
+ * Stable: Yes
+ *
+ * @param input: SortingInput
+ * @returns SortingOutput
+ */
 pub fn insertion_sort(input: SortingInput) -> SortingOutput {
+    // Initialize variables
     let mut arr = input.arr;
     let n = arr.len();
     let mut cycles: Vec<Cycle> = Vec::new();
 
+    // Iterate through the array, starting at the second element
     for i in 1..n {
+        // Initialize variables
         let mut steps = Vec::new();
-        let key = arr[i];
+        let key = arr[i]; // The key is the current element
         let mut j: isize = i as isize - 1;
+        // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
         while j >= 0 && arr[j as usize] > key {
+            // Shift the element to the right
             arr[j as usize + 1] = arr[j as usize];
+            // Add the step to the cycle
             steps.push(Step {
                 swapped: true,
                 compared: vec![(j as usize, j as usize + 1)],
             });
+            // Move to the next element
             j = j - 1;
         }
 
+        // If no swaps were made, add the comparison to the cycle
         if steps.is_empty() {
+            // Add the step to the cycle
             steps.push(Step {
                 swapped: false,
                 compared: vec![(j as usize, i)],
             });
         }
 
+        // Insert the key into the correct position
         arr[(j + 1) as usize] = key;
 
+        // Add cycle to cycles
         cycles.push(Cycle { cycle: steps });
     }
 
+    // If no cycles were made, add an empty cycle
     if cycles.is_empty() {
         cycles.push(Cycle { cycle: Vec::new() });
     }
 
+    // Return the output
     SortingOutput {
         sort: "Insertion".to_string(),
         cycles: cycles,
